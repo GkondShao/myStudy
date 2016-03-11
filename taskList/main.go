43709498b09ba2947d61ba87e21193ce608d17a4
Gkond <shao_rx@outlook.com>
2016-03-11 22:20:13 +0800

    ex

--- taskList/main.go
+++ taskList/main.go
@@ -10,6 +10,5 @@ func main() {
 
 	beego.Router("/", &controllers.MainController{})
 	beego.Router("/task/", &controllers.TaskController{}, "get:ListTasks;post:NewTask")
-	//beego.Router("/task/:id:int", &controllers.TaskController{}, "get:GetTask;put:UpdateTask")
 	beego.Run()
 }
